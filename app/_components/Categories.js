f6a2fbc8b181acf4af252f265dbc594f321c7a8f
Karim Shaaban <karimatef785@gmail.com>
2025-05-24 12:21:07 +0300

    test margin on categories

--- app/_components/Categories.js
+++ app/_components/Categories.js
@@ -1,9 +1,8 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
-import { motion } from "framer-motion"; // Import Framer Motion
-import { useSwipeable } from "react-swipeable"; // Import useSwipeable
+import { motion } from "framer-motion";
+import { useSwipeable } from "react-swipeable";
 
 const categories = [
   {
@@ -31,7 +30,7 @@ const categories = [
     name: "Soil quality monitoring",
     imgSrc: `${process.env.NEXT_PUBLIC_GALLERY}cat-6.png`,
   },
-    {
+  {
     id: 2,
     name: "Your Data is secure",
     imgSrc: `${process.env.NEXT_PUBLIC_GALLERY}cat-2.png`,
@@ -42,30 +41,23 @@ const Categories = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const visibleItems = 4;
 
-  // Handle Next button
   const handleNext = () => {
     if (currentIndex + visibleItems < categories.length) {
       setCurrentIndex((prev) => prev + 1);
     }
   };
 
-  // Handle Prev button
   const handlePrev = () => {
     if (currentIndex > 0) {
       setCurrentIndex((prev) => prev - 1);
     }
   };
 
-  // Swipe handlers
-  const handleSwipeLeft = () => handleNext();
-  const handleSwipeRight = () => handlePrev();
-
-  // Configure swipeable handlers
   const swipeHandlers = useSwipeable({
-    onSwipedLeft: handleSwipeLeft,
-    onSwipedRight: handleSwipeRight,
+    onSwipedLeft: handleNext,
+    onSwipedRight: handlePrev,
     preventDefaultTouchmoveEvent: true,
-    trackMouse: true, // Allow mouse swiping
+    trackMouse: true,
   });
 
   const variants = {
@@ -85,15 +77,21 @@ const Categories = () => {
       {...swipeHandlers}
     >
       <div className="relative flex gap-8 items-center justify-center">
-        {visibleCategories.map((category) => (
-          <motion.div
-            className="flex flex-col gap-4 font-semibold justify-center items-center"
-            key={category.id}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={variants} // Apply animation variants
-          >
+        {visibleCategories.map((category, index) => {
+          const isFirst = index === 0;
+          const isLast = index === visibleCategories.length - 1;
+
+          return (
+            <motion.div
+              className={`flex flex-col gap-4 font-semibold justify-center items-center ${
+                isFirst ? "ml-8" : ""
+              } ${isLast ? "mr-8" : ""}`}
+              key={category.id}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={variants}
+            >
               <div
                 className="flex-shrink-0 relative 2xl:h-48 2xl:w-48 lg:w-40 lg:h-40 md:w-36 md:h-36 sm:h-32 sm:w-32 xs:h-28 xs:w-28 bg-base-200 rounded-full shadow-lg"
                 style={{ borderRadius: "70% 30% 81% 19% / 21% 72% 28% 79%" }}
@@ -103,21 +101,22 @@ const Categories = () => {
                   alt={`${category.name} image`}
                   fill
                   priority
-                  style={{ objectFit: "scale-down" }} // Ensure images cover the div
+                  style={{ objectFit: "scale-down" }}
                 />
               </div>
               <div>
                 <h3 className="xs:text-xs text-center">{category.name}</h3>
               </div>
-          </motion.div>
-        ))}
+            </motion.div>
+          );
+        })}
       </div>
 
       {/* Prev Button */}
       {currentIndex > 0 && (
         <button
           onClick={handlePrev}
-          className=" hidden lg:block absolute top-1/2 left-8 z-50 active:scale-75 -translate-y-1/2 bg-white/50 p-2 rounded-full focus:outline-none hover:bg-gray-200 transform transition-transform duration-300 ease-out hover:scale-110"
+          className="hidden lg:block absolute top-1/2 left-8 z-50 active:scale-75 -translate-y-1/2 bg-white/50 p-2 rounded-full focus:outline-none hover:bg-gray-200 transform transition-transform duration-300 ease-out hover:scale-110"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -135,6 +134,7 @@ const Categories = () => {
           </svg>
         </button>
       )}
+
       {/* Next Button */}
       {currentIndex + visibleItems < categories.length && (
         <button
