7f20a8b413fcf8d640f1de3beb4d80ccae39a579
Karim Shaaban <karimatef785@gmail.com>
2025-05-24 09:16:51 +0300

    works

---
+++ app/_components/SkeletonDashboard.js
@@ -0,0 +1,14 @@
+import Skeleton from "@/components/ui/skeleton";
+import LoadingDots from "./LoadingDots";
+
+export default function SkeletonDashboard() {
+  return (
+    <div className="grid grid-cols-1 gap-6 p-8 h-[80svh]">
+      <Skeleton className="relative w-full h-full rounded-md bg-base-200 animate-pulse">
+        <div className="absolute inset-0 flex items-center justify-center">
+          <LoadingDots />
+        </div>
+      </Skeleton>
+    </div>
+  );
+}
