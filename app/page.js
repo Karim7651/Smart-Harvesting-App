2a457043d890ca05141328ab2de2f351bf14751e
Karim Shaaban <karimatef785@gmail.com>
2025-05-24 09:02:08 +0300

    test

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
 
 import Carousel from "./_components/Carousel";
 import Categories from "./_components/Categories";
-import SkeletonCard from "./_components/SkeletonCard";
-import SkeletonDashboardChart from "./_components/SkeletonDashboard";
-import SkeletonList from "./_components/SkeletonList";
+
 
 export default function Home() {
   return (
